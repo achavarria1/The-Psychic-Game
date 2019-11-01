f972aff88d632b2ba5180e0b5667a757fab411ed
allan chavarria <al.s.chavarria@gmail.com>
2019-11-01 13:47:41 -0700

    updated js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
             var lettersGuessed = null; // Will be initialized in gameInit()
             var randomLetterCode = null; // Will be initialized in gameInit()
 
-            var letterCodeMin = 65; // letter "a" TEST TEST
+            var letterCodeMin = 65; // letter "a" TEST 
             var letterCodeMax = 90; // letter "z"
 
             document.onkeyup = function(e) {
